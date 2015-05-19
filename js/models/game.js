a496b93dadc4d2c22d67a450853fe64a7047dd67
stephaniecoleman <sproutwingsandfly@gmail.com>
2015-05-19 17:45:08 -0400

    making way through lab.

--- js/models/game.js
+++ js/models/game.js
@@ -10,23 +10,85 @@
 								 [0,3,6], [1,4,7], [2,5,8], 
 								 [0,4,8], [2,4,6]]
 		},
+
+		hasMark: function(id){
+			if (this.get("board")[id]){
+				return true;
+			}
+		},
+
 		updateState : function() {
-			
+			if (this.hasMark(id)){
+				return false;
+			} else {
+				var board = this.get("board")
+				board[id] = this.player();
+				this.set("board", board);
+				this.trigger(this.player(), id);
+			}
+			return true;
 		},
+
+
 		tie : function() {
-		
+			for (var i = 0; i < this.get("board").length; i++) {
+				if (!this.hasMark(i)) {
+					return false;
+				}
+			}
+			return true;
 		},
+
+		checkFor3: function(combo) {
+			for(var i = 0; i < combo.length; i++) {
+				if (this.get("board")[combo[i]] != this.player()) {
+					return false;
+				}
+			}
+			return true;
+		},
+
 		gameOver : function() {
-			
+			if (this.winnerCheck()) {
+				this.set("endStatus", "win");
+				return true;
+			} else if (this.tie()) {
+				this.set("endStatus", "tie");
+				return true;
+			} else {
+				return false;
+			}
 		},
+
 		player : function() {
-			
+			for(var i = 0; i < this.get("winningCombos").length; i++) {
+				if (this.checkFor3(this.get("winningCombos")[i]) == true) {
+					return true;
+				}
+			}
+			return false;
 		},
-		checkCells : function() {
-			
+
+		addTurn: function(){
+			var turn = this.get("turn");
+			this.set("turn", turn + 1);
 		},
+
 		doTurn : function() {
-			
+			// Add mark to virtual board
+			if (this.updateState(id)){
+				
+				// Check for winner
+				if (this.gameOver()) {
+					// Alert for winner (view)
+					this.trigger(this.get("endStatus"), this.player());
+				}
+
+				// Increment turn
+				this.addTurn();
+			}
 		}	
+
+
 	})
 })();
--- js/views/gameview.js
+++ js/views/gameview.js
@@ -2,33 +2,57 @@
 	var GameView = app.GameView = Backbone.View.extend({
 
 		events: {
-			"click" : "handleClick"
+			"click table" : "handleClick",
+			"click #lastGame" : "showLastGame"
 		},
 
-		initialize : function() {
-
+		initialize: function() {
+			this.game = new app.Game();
+			this.game.on("X", this.drawX, this);
+			this.game.on("O", this.drawO, this);
+			this.game.on("win", this.win, this);
+			this.game.on("tie", this.tie, this);
+			this.render();
+			// wire up event listeners here
 		},
 
-		render : function() {
+		render: function() {
+			var html = '<div id="message"></div><table border="1" cellpadding="40"><tbody><tr><td id="0"></td><td id="1"></td><td id="2"></td></tr><tr><td id="3"></td><td id="4"></td><td id="5"></td></tr><tr><td id="6"></td><td id="7"></td><td id="8"></td></tr></tbody></table>';
+      this.$el.append(html);
+      $("#container").append(this.$el);
 		},
 
-		addIds : function() {
-
+		handleClick: function(event) {
+			var id = parseInt(event.target.id);
+			this.game.doTurn(id);
 		},
 
-		handleClick : function() {
-
+		drawX: function(id) {
+			$('td#' + id).html("X")
 		},
 
-		tie : function() {
-	
+		drawO: function(id) {
+			$('td#' + id).html("O")
 		},
 
-		win : function() {
-
+		tie: function() {
+			$('#message').text("Tie game");
+			this.clearBoard();
 		},
 
-		clearBoard : function() {
+		win: function(player) {
+			$('#message').text("Player " + player + " Wins!");
+			this.clearBoard();
+		},
+
+		showLastGame: function(){
+			debugger;
+		},
+
+		clearBoard: function() {
+			$('td').text("");
+			this.game.set("board", [null, null, null, null, null, null, null, null, null]);
+			this.game.set("turns", 0);
 		}
 	})
 })();
